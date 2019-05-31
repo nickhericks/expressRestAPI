58d42ea4142fce481f8de600d713e99f40831d6c
Nick-Hericks <nickhericks@gmail.com>
2019-05-31 17:28:53 +0200

    Add delete quote route

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
+const records = require('./records');
 
-
-
-
-
+// Middleware that tells express incoming data should be read as json
+// and also that it should be made available in the req.body
+app.use(express.json());
 
 
 // get method takes two arguments
@@ -17,52 +17,90 @@ app.get('/greetings', (req, res) => {
 	res.json({greeting: 'Hello world!'});
 });
 
-// Send a GET request to READ(view) a list of quotes
-app.get('/quotes', (req, res) => {
-	res.json( data );
+
+
+
+
+
+// Send a GET request to /quotes to READ(view) a list of quotes
+app.get('/quotes', async (req, res) => {
+	const quotes = await records.getQuotes();
+	res.json(quotes);
 });
 
-// Send a GET request to READ(view) a quote
-app.get('/quotes/:id', (req, res) => {
-	res.json(data);
+// Send a GET request to /quotes/:id to READ(view) a quote
+app.get('/quotes/:id', async (req, res) => {
+	try {
+		// throw new Error('Oh noooooooo!');
+		const quote = await records.getQuote(req.params.id);
+		if(quote) {
+  		res.json(quote);
+		} else {
+			res.status(404).json({message: 'Quote not found.'});
+		}
+	} catch(err) {
+		res.status(500).json({error: err.message});
+	}
 });
 
-// Send a POST request to CREATE a new quote
+// Send a POST request to /quotes to CREATE a new quote
+app.post("/quotes", async (req, res) => {
+	try {
+		// throw new Error('Oh noooooooo!');
+		if(req.body.author && req.body.quote) {
+			const quote = await records.createQuote({
+				quote: req.body.quote,
+				author: req.body.author
+			});
+			res.status(201).json(quote);
+		} else {
+			res.status(400).json({message: 'Quote and author required.'});
+		}
+	} catch(err) {
+		res.status(500).json({error: err.message});
+	}
+  
+});
 
+// Send a PUT request to /quotes/:id to UPDATE a quote
+app.put('/quotes/:id', async (req, res) => {
+	try {
+		// throw new Error('Oh noooooooo!');
+		const quote = await records.getQuote(req.params.id);
+		if(quote) {
+			quote.quote = req.body.quote;
+			quote.author = req.body.author;
+			await records.updateQuote(quote);
+			// We don't send anything back with PUT requests,
+			// so we use Express .end() method to tell it we're done.
+			res.status(204).end();
+		} else {
+			res.status(404).json({ message: "Quote not found." });
+		}
+	} catch(err) {
+		res.status(500).json({ error: err.message });
+	}
+});
 
-// Send a PUT request to UPDATE a quote
+// Send a DELETE request to /quotes/:id to DELETE a quote
+app.delete("/quotes/:id", async (req, res) => {
+  try {
+    // throw new Error('Oh noooooooo!');
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+      await records.deleteQuote(quote);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: "Quote not found." });
+    }
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
-
-// Send a DELETE request to DELETE a quote
-
-
-// Send a GET request to READ(view) a random quote
+// Send a GET request to /quotes/quote/random to READ(view) a random quote
 
 
 
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-
-const data = {
-	quotes: [
-		{
-			id: 8721,
-			quote:
-        'We must accept finite disappointment, but we must never lose infinite hope.',
-			author: 'Martin Luther King'
-		},
-		{
-			id: 5779,
-			quote:
-        'Use what you’ve been through as fuel, believe in yourself and be unstoppable!',
-			author: 'Yvonne Pierre'
-		},
-		{
-			id: 3406,
-			quote:
-        'To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.',
-			author: 'Barbara DeAngelis'
-		}
-	]
-};
